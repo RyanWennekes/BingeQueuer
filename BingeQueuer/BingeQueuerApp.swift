595c2e1fca1afe4d4ca6062b995146b6241fe2b0
Ryan <Ryan.Wennekes@hotmail.com>
2022-02-09 11:18:52 +0100

    Initial Commit

---
+++ BingeQueuer/BingeQueuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingeQueuerApp.swift
+//  BingeQueuer
+//
+//  Created by Ryan Wennekes on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BingeQueuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
